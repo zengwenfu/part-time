0498dd661320e390a01bc49bee80acc22863c247
huangchucai <425205839@qq.com>
2017-10-28 22:31:23 +0800

    动态数据

--- src/police/js/popup.js
+++ src/police/js/popup.js
@@ -1,7 +1,149 @@
 /**
  * Created by Z7 on 2017/10/27.
  */
+var data = {
+  await: [
+    [
+      { id: 'No.20171018032A', name: '全部', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉疆重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '网上逃犯', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉毒人员', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '流窜重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ]
+  ],
+  push: [
+    [
+      { id: 'No.20171018032A', name: '全部', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '推送', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉疆重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '推送', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '网上逃犯', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '推送', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉毒人员', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '推送', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '流窜重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '推送', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ]
+  ],
+  deal: [
+    [
+      { id: 'No.20171018032A', name: '全部', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '处理', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉疆重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '处理', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '网上逃犯', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '处理', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '涉毒人员', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '处理', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ],
+    [
+      { id: 'No.20171018032A', name: '流窜重点', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '处理', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' },
+      { id: 'No.20171018032A', name: '赵钱孙', cardId: '340522196709272789', props: '网络逃犯', content: '内容备注在这里内容备注在这里内容备注在这里' }
+    ]
+  ]
+};
+function changeTabel(panelFirst, index) {
+  var tbody = '';
+  var TableType;
+  if (panelFirst === 0) {
+    TableType = 'await'
+  } else if (panelFirst === 1) {
+    TableType = 'push'
+  } else if (panelFirst === 2) {
+    TableType = 'deal'
+  }
+
+  var length = data[TableType][index].length;
+  for (var i = 0; i < length; i++) {
+    tbody += '<tr>' +
+      '<td><input type="checkbox"></td>' +
+      '<td>' + data[TableType][index][i].id + '</td>' +
+      '<td>' + data[TableType][index][i].name + '</td>' +
+      '<td>' + data[TableType][index][i].cardId + '</td>' +
+      '<td>' + data[TableType][index][i].props + '</td>' +
+      '<td>' + data[TableType][index][i].content + '</td>' +
+      '<tr>'
+  };
+  if (panelFirst === 0) {
+    $('.popup-tbody-1').html(tbody);
+  } else if (panelFirst === 1) {
+    $('.popup-tbody-2').html(tbody);
+  } else if (panelFirst === 2) {
+    $('.popup-tbody-3').html(tbody);
+  }
+}
 $(function () {
+  var panelFirst = 0;
+  changeTabel(panelFirst, 0)
   $('.container-popup-first .push-panel').on('click', function () {
     $('.container-popup-first').hide();
     $('.container-popup-two').show();
@@ -14,16 +156,21 @@ $(function () {
     $('.container-popup-two').hide();
     $('.container-popup-first').hide();
   });
-  // 轮播
+  // 切换一级菜单
   $('.container-popup-first .nav li').on('click', function () {
-    var index = $(this).index();
+    panelFirst = $(this).index();
+    changeTabel(panelFirst, 0)
     $(this).siblings().removeClass("active");
     $(this).addClass('active');
     $(this).parents(".container-popup-first").find(".content-popup").removeClass("show");
-    $(this).parents(".container-popup-first").find(".content-popup").eq(index).addClass("show");
+    $(this).parents(".container-popup-first").find(".content-popup").eq(panelFirst).addClass("show");
   })
   // 切换二级菜单
-  $('.button-popup .btn').on('click', function(){
+  $('.button-popup .btn').on('click', function () {
+    var index = $(this).index();
+    var tbody = '';
+    var length;
+    changeTabel(panelFirst, index);
     $(this).siblings().removeClass("first");
     $(this).addClass('first');
   })
